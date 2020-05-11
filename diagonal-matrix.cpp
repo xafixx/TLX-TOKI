d4a4afc545c49d36f50605bb9ea1dec636693562
M.Fikri Aqsha Zulfa Ismail <56661390+xafixx@users.noreply.github.com>
2020-05-11 13:32:21 +0700

    Update diagonal-matrix.cpp

--- diagonal-matrix.cpp
+++ diagonal-matrix.cpp
@@ -18,16 +18,17 @@ using namespace std;
 int main() {
 	
 	// init array 3 x 3
-	int arr[3][3] = {
-						{1 ,2, 3}, 
-						{4, 5, 6},
-						{7, 8, 9}
-					};
+	int arr[3][3] = { 
+			  {1 ,2, 3}, 
+			  {4, 5, 6},
+			  {7, 8, 9}
+			};
+	
 	// print matriks
 	for(int i = 0; i < 3; i++) {
 		for(int j = 0; j < 3; j++) {
 			if(i != j) {
-				arr[i] [j] = 0;
+			   arr[i] [j] = 0;
 			}
 			cout <<" "<< arr[i] [j];
 		}
